242b1f88eae915b81c5e56e8a9e054793928933f
mhdfahad1 <mhdfahad53@gmail.com>
2023-12-30 14:31:16 +0530

    edited

--- src/services/BaseUrl.js
+++ src/services/BaseUrl.js
@@ -1,2 +1,2 @@
-export const Base_url='http://localhost:4000'
-// export const Base_url='https://tg-server-4aqw.onrender.com'
+// export const Base_url='http://localhost:4000'
+export const Base_url='https://tg-server-4aqw.onrender.com'
